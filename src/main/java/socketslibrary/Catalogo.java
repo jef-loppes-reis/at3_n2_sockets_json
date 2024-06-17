505ab448bda27fccbfc35a7877272b65ae6bcc25
Pedro-Chaves2505 <pedrochaves_10@live.com>
2024-06-17 14:32:23 -0300

    feat: adicionada funcionalidade de listar os livros
    O problema é que está sendo retornada em formato JSON. Seria adequado inserir metodo toCliente para retornar de uma forma mais adequada para ele

--- src/main/java/socketslibrary/Catalogo.java
+++ src/main/java/socketslibrary/Catalogo.java
@@ -7,74 +7,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.exc.StreamWriteException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Catalogo {
-    private List<OpcaoDeLivro> opcoesDeLivros;
+    private List<OpcaoDeLivro> opcoesDeLivros = new ArrayList<OpcaoDeLivro>();
+    private File arquivoLivrosJSON;
 
-    public Catalogo(String pathLivrosJSON) {
-        this.opcoesDeLivros = new ArrayList<OpcaoDeLivro>();
-        String jsonString;
-        try {
-            File arquivoCatalogo  = new File(pathLivrosJSON);
-            jsonString = new String(Files.readAllBytes(arquivoCatalogo.toPath()));
-
-            ObjectMapper mapper = new ObjectMapper();       
-            try {
-                JsonNode jsonNode = mapper.readTree(jsonString);
-                // JsonNode arrayNode = jsonNode.get("livros");
-
-                if (jsonNode.isArray()) {
-                    for (JsonNode node : jsonNode) {
-                        String titulo = node.get("titulo").asText();
-                        String autor = node.get("autor").asText();
-                        String genero = node.get("genero").asText();
-                        int exemplares = node.get("numeroDeExemplares").asInt();
-
-                        OpcaoDeLivro opcaoDeLivroAtual = new OpcaoDeLivro(titulo, 
-                        autor,
-                        genero,
-                        exemplares);
-
-                        this.opcoesDeLivros.add(opcaoDeLivroAtual);
-                    }
-                    System.out.println(this.opcoesDeLivros);
-                }
-
-            } catch (JsonMappingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (JsonProcessingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }    
+    public Catalogo(String pathLivrosJSON) throws IOException {
+        this.arquivoLivrosJSON = new File(pathLivrosJSON);
+        String jsonString = new String(Files.readAllBytes(this.arquivoLivrosJSON.toPath()));
+        this.opcoesDeLivros = conversorLivrosFormatoJSONparaFormatoListaOpcaoDeLivro(jsonString);
     }
 
-    public void salvar(String pathLivrosJSON) {
-        ObjectMapper mapper = new ObjectMapper();  
-        File arquivoCatalogo  = new File(pathLivrosJSON);
-       
-        try {
-            mapper.writerWithDefaultPrettyPrinter()
-                    .writeValue(arquivoCatalogo, this.opcoesDeLivros);
-        } catch (StreamWriteException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (DatabindException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    public String toJSON() {
+        String catalogoJSON = "{\n\t\"livros\":\n[\n";
+        for (OpcaoDeLivro opcaoDeLivro: this.opcoesDeLivros) {
+            catalogoJSON += opcaoDeLivro.toJSON() + "\n";
         }
+        catalogoJSON += "]\n}";
+        return catalogoJSON;
     }
+
+    public List<OpcaoDeLivro> conversorLivrosFormatoJSONparaFormatoListaOpcaoDeLivro(String catalogoJSON) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();       
+        JsonNode jsonNode = mapper.readTree(catalogoJSON);
+        jsonNode = jsonNode.get("livros");
+
+        List<OpcaoDeLivro> opcoesDeLivrosTemporaria = new ArrayList<OpcaoDeLivro>();
+
+        if (jsonNode.isArray()) {
+            for (JsonNode node : jsonNode) {
+                String titulo = node.get("titulo").asText();
+                String autor = node.get("autor").asText();
+                String genero = node.get("genero").asText();
+                int exemplares = node.get("numero_de_exemplares").asInt();
+
+                OpcaoDeLivro opcaoDeLivroAtual = new OpcaoDeLivro(titulo, 
+                autor,
+                genero,
+                exemplares);
+
+                opcoesDeLivrosTemporaria.add(opcaoDeLivroAtual);
+            }
+        }
+        return opcoesDeLivrosTemporaria;
+    }
+
 }
--- src/main/java/socketslibrary/Servidor.java
+++ src/main/java/socketslibrary/Servidor.java
@@ -4,28 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
     public static void main(String[] args) throws Exception {
+        boolean continuar = true;
         try{
             ServerSocket serverSocket = new ServerSocket(4000);
-                       
-            while (true) {
-                Socket socket = serverSocket.accept();
-                ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
-                String recebido = entrada.readObject().toString();    
+            Socket socket = serverSocket.accept();
+            Catalogo catalogo = new Catalogo("data/livros.json");
+
+            while (continuar) {
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                String recebido = objectInputStream.readObject().toString();    
+                
                 switch (recebido) {
                     case "1":
-                        System.out.println("estou aqui!"); 
+                        System.out.println("ok");
+                        objectOutputStream.writeObject(String.valueOf(catalogo.toJSON()));
+                        break;
+                    case "4":
+                        continuar = false;
                         break;
                     default:
                         break;
                 }
-                socket.close();
             }
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         } 
